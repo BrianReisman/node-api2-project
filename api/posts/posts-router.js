6e79bbde1c27fbaa0aa0e996ebe48a9552aeb7f8
BrianReisman <brianreisman@protonmail.com>
2021-03-12 10:49:30 -0500

    [GET] at '/' working and connected with DB

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,37 @@
 // implement your posts router here
+const router = require('express').Router()
+const Posts = require('./posts-model');
+
+router.get('/', (req,res)=>{
+  Posts.find()
+    .then(data =>{
+      console.log(data)
+      res.status(200).json(data)
+    })
+    .catch(err => {
+      res.status(500).json({ message: "The posts information could not be retrieved" })
+      console.log(err)
+    })
+})
+
+router.post('/', (req, res)=>{
+  console.log('.post() within router')
+})
+
+router.get('/:id', (req,res)=>{
+  console.log('test test from get w/:id')
+})
+
+router.put('/:id', (req,res)=>{
+  console.log('time to [PUT]')
+})
+
+router.delete('/:id', (req,res)=>{
+  console.log('DELETE THEM!!!!!!!!!')
+})
+
+router.get('/:id/comments', (req,res)=>{
+  console.log('id with comments, we\'re getting fancy here')
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -2,9 +2,17 @@
 // require your posts router and connect it here
 
 const express = require('express');
+
 const server = express()
+const postsRouter = require('./posts/posts-router')
+
+//MIDDLEWARE
+server.use(express.json())
+server.use('/api/posts', postsRouter)
 
 
-
+server.get('/', (req, res) => {
+  console.log('test .get()')
+})
 
 module.exports = server;
