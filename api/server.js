a909615e345312ecaf24fe3940b6aed5bbbd8515
BrianReisman <brianreisman@protonmail.com>
2021-03-14 15:16:25 -0400

    final commit MVP met all endpoints working

--- api/server.js
+++ api/server.js
@@ -1,18 +1,13 @@
+
 // implement your server here
-// require your posts router and connect it here
-
 const express = require('express');
-
 const server = express()
+
+// require your posts router and connect it here
 const postsRouter = require('./posts/posts-router')
 
 //MIDDLEWARE
 server.use(express.json())
 server.use('/api/posts', postsRouter)
-
-
-server.get('/', (req, res) => {
-  console.log('test .get()')
-})
 
 module.exports = server;
