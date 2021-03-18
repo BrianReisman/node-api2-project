463e904f38efbf50d53d6b6776de913c2cc4405e
BrianReisman <brianreisman@protonmail.com>
2021-03-17 23:11:36 -0400

    changed version of sqlite3

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // require your server and launch it here
 const server = require("./api/server");
-require('dotenv').config()
-
-const PORT = process.env.PORT;
+require('dotenv').config() //
+const PORT = process.env.PORT; //
 
 server.listen(PORT, () => {
   console.log(`\n *** server listening on ${PORT} *** \n`);
