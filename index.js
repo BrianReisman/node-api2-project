37dabfdc65c39b3aac288a4f6f16fc39a6d5feb3
BrianReisman <brianreisman@protonmail.com>
2021-03-17 22:41:35 -0400

    added dotenv and .env with port for index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 // require your server and launch it here
-const server = require('./api/server');
+const server = require("./api/server");
+require('dotenv').config()
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
-server.listen(PORT, ()=>{
-  console.log(`\n *** server listening on ${PORT} *** \n`)
-})
+server.listen(PORT, () => {
+  console.log(`\n *** server listening on ${PORT} *** \n`);
+});
